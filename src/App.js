d0ebe91288569c6651bab467c67bd15ec68aba2b
Saba Kauser <sabakauser411@gmail.com>
2023-06-07 19:12:57 +0100

    formatted code

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
 import React, { useState } from "react";
 import Header from "./components/Header/header";
-import EmployeeList from "./components/EmployeeList/employeeList"
+import EmployeeList from "./components/EmployeeList/employeeList";
 import EmployeeDetails from "./components/EmployeeDetails/employeeDetails";
-import data from "./data/EmployeeData"
+import data from "./data/EmployeeData";
 
 const App = () => {
   const [currentId, setCurrentId] = useState(0);
   const [employees, setEmployees] = useState(data.employees);
 
   const handleChangeEmployee = (id) => () => {
-    setCurrentId(id)
-  }
+    setCurrentId(id);
+  };
 
   const updatePopularity = (e) => {
-    const newData = JSON.parse(JSON.stringify(employees))
-    newData[currentId].popularity = Number(e.currentTarget.value)
+    const newData = JSON.parse(JSON.stringify(employees));
+    newData[currentId].popularity = Number(e.currentTarget.value);
     setEmployees(newData);
-  }
+  };
 
   return (
     <div className="app">
-        <EmployeeList currentId={currentId} employee={employees} onChange={handleChangeEmployee}/>
+      <EmployeeList
+        currentId={currentId}
+        employee={employees}
+        onChange={handleChangeEmployee}
+      />
       <div>
         <Header />
-        <EmployeeDetails employee={employees[currentId]} currentId={currentId} onChangePopularity={updatePopularity} />
+        <EmployeeDetails
+          employee={employees[currentId]}
+          currentId={currentId}
+          onChangePopularity={updatePopularity}
+        />
       </div>
     </div>
   );
--- src/components/EmployeeList/employeeList.js
+++ src/components/EmployeeList/employeeList.js
@@ -1,17 +1,30 @@
-import React from "react"
-import Logo from '../../assets/_logo/the-godfather.svg';
-import "./employeeList.scss"
+import React from "react";
+import Logo from "../../assets/_logo/the-godfather.svg";
+import "./employeeList.scss";
 
-const EmployeeList = ({currentId, employee, onChange}) => {
+const EmployeeList = ({ currentId, employee, onChange }) => {
   return (
     <div className="employee-list">
-      <div className="employee-list__logo"><img src={Logo} alt="logo" /></div>
+      <div className="employee-list__logo">
+        <img src={Logo} alt="logo" />
+      </div>
       <div className="employee-list__container">
         {employee?.length > 0 &&
-          employee.map((item, id) => <div key={id} style={{fontSize : `${employee[id].popularity * 0.8}rem`}} className={`employee-list__container__item ${currentId === id && 'active'}` } onClick={onChange(id)}>{item?.name}</div>)}
+          employee.map((item, id) => (
+            <div
+              key={id}
+              style={{ fontSize: `${employee[id].popularity * 0.8}rem` }}
+              className={`employee-list__container__item ${
+                currentId === id && "active"
+              }`}
+              onClick={onChange(id)}
+            >
+              {item?.name}
+            </div>
+          ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default EmployeeList
+export default EmployeeList;
