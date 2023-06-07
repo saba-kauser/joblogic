ff32d65de0911def05f275a8a14a637f3d401bb6
Saba Kauser <sabakauser411@gmail.com>
2023-06-07 12:13:07 +0100

    modified app.js  and index.js file , deleted manifest.json,app.css  index.css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+    <div className='app'>
+        
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
